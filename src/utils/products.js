e098f009e61ca71dab1e4754c110dc5e18c77e9b
Guilherme <guilhermebr328@gmail.com>
2025-09-03 13:07:46 -0300

    Adição de produtos

--- src/utils/products.js
+++ src/utils/products.js
@@ -10,14 +10,14 @@ const products = [
         image: "https://tfcprmt.vteximg.com.br/arquivos/ids/168013-1000-1000/16aVHzUlVZRyAD8LGrwchzVzfN96IhVg.jpg?v=637813101314100000"
     },
     {
-        name: "Micrômetro digital de precisão",
-        price: 449.49,
-        image: "https://ae01.alicdn.com/kf/H40c0d80c0c3e48a7a7429f7a944c9224p/Micr-metro-eletr-nico-digital-de-alta-precis-o-0-25mm-cabe-as-de-0-001mm.jpg"
+        name: "Fedor do Bahia",
+        price: 0.01,
+        image: "https://img.freepik.com/fotos-premium/representacion-3d-imagen-fondo-concepto-barril-desechos-nucleares_651547-1386.jpg"
     },
     {
-        name: "Fogareiro portátil a álcool sólido",
-        price: 32.25,
-        image: "https://tse2.mm.bing.net/th/id/OIP.LdfqndQ9uV0zoae2vTEbHAHaHa?r=0&rs=1&pid=ImgDetMain&o=7&rm=3"
+        name: "Cérebro do Kleberson",
+        price: 0,
+        image: ""
     },
     {
         name: "Beringela do Fábio Leonardo",
@@ -28,7 +28,68 @@ const products = [
         name: "Ronaldo",
         price: 69.69,
         image: "https://media.tenor.com/c-fmfYiYatEAAAAM/ronaldo.gif"
-    }
+    },
+    {
+        name: "Fio de Cobre pra Lâmpada Incandescente",
+        price: 29.99,
+        image: "https://thaka.bing.com/th/id/OIP.71tEg2mc9fyc6ON69s_ZbQHaGM?w=189&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3"
+    },
+    {
+        name: "Suco de Laranja",
+        price: 6.9,
+        image: "https://tudoegostoso.com.br/wp-content/uploads/2023/11/suco-de-laranja-768x443.jpg"
+    },
+    {
+        name: "Lacinho da Bombeta Azul Bebê",
+        price: 12000,
+        image: "https://http2.mlstatic.com/D_NQ_NP_685936-MLB49365305872_032022-O.webp"
+    },
+    {
+        name: "Unha postiça do dedão esquerdo de alguém",
+        price: 34,
+        image: "https://1.bp.blogspot.com/-NcAjFzjJMWM/T1iwnRRxDaI/AAAAAAAABbw/KrEpMuY5j9A/s1600/Imagem+116.jpg"
+    },
+    {
+        name: "Dedo perdido do Lula",
+        price: 1313,
+        image: "https://tse1.mm.bing.net/th/id/OIP.Ch83BDhL9iD9X7rQsZW_QAHaHa?r=0&rs=1&pid=ImgDetMain&o=7&rm=3"
+    },
+    {
+        name: "Ovo perdido do Bolsonaro",
+        price: 1722,
+        image: "https://veja.abril.com.br/wp-content/uploads/2019/01/ovo-1.png"
+    },
+    {
+        name: "Desenho de um ônibus feito no Rio de Janeiro",
+        price: 24000000,
+        image: "https://img.freepik.com/fotos-premium/um-onibus-colorido-pintura-em-aquarela-arte-digital-um-onibus-vermelho-de-dois-andares-generative-ai_894903-4298.jpg?w=2000"
+    },
+    {
+        name: "Ônibus 6835-10",
+        price: 5,
+        image: "https://i.ytimg.com/vi/K0hsZbLK6K0/hqdefault.jpg"
+    },
+    {
+        name: "Ônibus 677Y-10",
+        price: 5,
+        image: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj1oQ_mmFvNe-SzYUABZv4b0WMLCcr_Hbj-N9CWolBccS6c_I_ozXn6UeCu4hK9y9X4suvUa_6vIaojS8CWby7ml697TL5HC2jVyR_UXpWbKtkikq0-l8fo6AaNdVtV-XM_-qhB5oj1WPt-rpdlKEzVWEUyK8Tm6sw3EBTpIt3cDbaz7DS0TwBUUx5XUSwk/s1080/FB_IMG_1736432371580.jpg"
+    },
+    {
+        name: "Mola do amortecedor frontal direito do Pálio 2010 1.0 8V",
+        price: 10,
+        image: "https://images.tcdn.com.br/img/img_prod/1142409/mola_amortecedor_dianteiro_6_voltas_14mm_jeep_compass_2016_2021_diesel_5975_1_d56d680df89f9ffcd1afb27432fae15a.jpg"
+    },
+    {
+        name: "LED da Placa de Vídeo do PC RTX 5090 com Roblox Instalado",
+        price: 5090,
+        image: "https://images.tcdn.com.br/img/img_prod/645987/fita_led_14_4w_smd5050_ip20_12v_branco_quente_2700k_rolo_5m_647_1_20201213220018.jpg"
+    },
+    {
+        name: "Átomo do fio de cabelo do Kleberson Reginaldo (nossa maior relíquia)",
+        price: 9999999999999.99,
+        image: "https://th.bing.com/th/id/R.3e95452d23416056976746d2628c33b0?rik=ULz6uH9Hu1SPTA&riu=http%3a%2f%2fespaciociencia.com%2fwp-content%2fuploads%2fatomo-portada2.jpg&ehk=RalSz%2fHSX3zEisThuxucQFrE7ehcaZvvoLng69DWwK8%3d&risl=&pid=ImgRaw&r=0"
+    },
+
 ];
 
 export default products;
